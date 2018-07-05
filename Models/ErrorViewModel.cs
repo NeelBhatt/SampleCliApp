583c9ada419596c39d3ff7ffe202d0bcee8e67d7
Bhatt, Neel <Neel.bhatt@mastercard.com>
2018-07-05 17:45:37 +0530

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SampleCliApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
